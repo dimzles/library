8ad9605b74e0bc5bccb48235ce8ef0496aa4b041
dimzles <dimzlestv@gmail.com>
2022-12-07 23:38:02 +0000

    Add cards displaying book info

--- javascript.js
+++ javascript.js
@@ -1,5 +1,8 @@
 const shelf = document.getElementById('shelf');
 const theHobbit = new Book('The Hobbit', 'J.R.R Tolkien', '295 pages', false);
+const harryPotter = new Book('Harry Potter and the Philosphers Stone', 'J. K. Rowling', '223 pages', true);
+const howToWinFriends = new Book('How to Win Friends and Influence People', 'Dale Carnegie', '275 pages', false);
+const crushingIt = new Book('Crushing It!', 'Gary Vaynerchuk', '288 pages', false);
 
 let myLibrary = [];
 
@@ -14,4 +17,21 @@ Book.prototype.info = function () {
     return `${this.title} by ${this.author}, ${this.pages}, ${this.readStatus ? "read" : "unread"}.`
 }
 
-console.log(theHobbit.info());
+function addBookToLibrary(...books) {
+    return myLibrary.push(...books);
+}
+
+function createBooks() {
+    for(let i = 0; i < myLibrary.length; i++) {
+        let card = document.createElement('div');
+        card.classList.add('card');
+        shelf.appendChild(card);
+        let cardText = document.createElement('p');
+        cardText.classList.add('card-text');
+        card.appendChild(cardText);
+        cardText.textContent = `${myLibrary[i].info()}`;
+    }
+}
+
+addBookToLibrary(theHobbit, harryPotter, howToWinFriends, crushingIt);
+createBooks();
